f78b58585bcbfd3355b5fc9c0fcaaf920103c089
Franz Gollhammer <33837460+franzgollhammer@users.noreply.github.com>
2023-10-04 11:24:32 +0200

    feat: semver script

---
+++ upgrade-package-version.js
@@ -0,0 +1,9 @@
+const packageJSON = require('./package.json')
+const fs = require('fs')
+
+const version = packageJSON.version.split('.')
+version[2] = parseInt(version[2]) + 1
+packageJSON.version = version.join('.')
+fs.writeFileSync('./package.json', JSON.stringify(packageJSON, null, 2))
+console.log(`Upgraded package version to ${packageJSON.version}`)
+
